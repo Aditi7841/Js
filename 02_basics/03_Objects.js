f05ff0f30621a773d17feb876119054898962d4c
Aditi7841 <65119842+Aditi7841@users.noreply.github.com>
2025-02-06 06:41:49 +0000

    objects

---
+++ 02_basics/03_Objects.js
@@ -0,0 +1,30 @@
+//Singletone
+//can be created of only constructor
+
+// Object Literals
+const MySym = Symbol('key1');
+const MyObj = {
+    name: 'ABC',
+    age: 22,
+    [MySym]: 'key1', // using symbol inside object
+    "address": 'Pune',
+    isLoggedIn: true
+}
+
+// console.log(MyObj['address']);
+// console.log(MyObj.name);
+// console.log(MyObj[MySym]);
+// console.log(MyObj);
+
+MyObj.greetings = function()
+{
+    console.log('Hello Js user');
+}
+
+MyObj.greetingsTwo = function()
+{
+    console.log(`Hello js user, ${this.name}`) // string interpolation
+}
+
+console.log(MyObj.greetings())
+console.log(MyObj.greetingsTwo())
